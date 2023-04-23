ba6e5a69b2011d6615a1d1052195f6da665cbca7
advasileva <alenadvasileva@yandex.ru>
2023-04-23 06:11:35 +0300

    fix program

--- 4-points/main.c
+++ 4-points/main.c
@@ -14,40 +14,60 @@
 #include <sys/mman.h>
 #include <string.h>
 
-void seller(sem_t *sem, char *addr, char *name) {
+void seller(sem_t *sem, char *addr, int id) {
     printf("Seller PID: %d\n", getpid());
 
-    while (addr[0] == 1) {
+    while (addr[0] == 1
+    ) {
         sem_wait(sem);
 
-        printf("Selling stock by %s\n", name);
+        if (addr[id] != 0)
+        {
+            printf("Selling stock by %d\n", id);
+            addr[id] = 0;
+        }
         sleep(1);
 
         sem_post(sem);
     }
+    printf("Seller %d finished\n", id);
 }
 
 void buyer(int *list[], int size, sem_t *sem_first, sem_t *sem_second, char *addr) {
     printf("Buyer PID: %d\n", getpid());
 
     for (size_t i = 0; i < size; i++)
-    {
+    {   
+        // addr[0] = 1;
         if (i % 2 == 0) {
             sem_wait(sem_first);
 
-            printf("Buying stock\n");
+            if (addr[1] == 0) {
+                printf("Buying stock from 1\n");
+                addr[1] = 1;
+            } else {
+                i--;
+            }
+            
             sleep(1);
 
             sem_post(sem_first);
         } else {
             sem_wait(sem_second);
 
-            printf("Buying stock\n");
+            if (addr[2] == 0) {
+                printf("Buying stock from 2\n");
+                addr[2] = 1;
+            } else {
+                i--;
+            }
+
             sleep(1);
 
             sem_post(sem_second);
         }
     }
+    addr[0] = 0;
 }
 
 int main(int argc, char **argv) {
@@ -60,17 +80,19 @@ int main(int argc, char **argv) {
     ftruncate(shm, size);
     addr = mmap(0, size, PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0);
     addr[0] = 1;
+    addr[1] = 0;
+    addr[2] = 0;
 
     sem_first = sem_open("first", 0);
     sem_second = sem_open("second", 0);
     
     int id = fork();
     if (id == 0) {
-        seller(sem_first, addr, "first");
+        seller(sem_first, addr, 1);
     } else {
         id = fork();
         if (id == 0) {
-            seller(sem_second, addr, "second");
+            seller(sem_second, addr, 2);
         }
         else {
             id = fork();
@@ -79,8 +101,6 @@ int main(int argc, char **argv) {
             }
         }
     }
-
-    addr[0] = 0;
 
     shm_unlink(memn);
 
